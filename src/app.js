a65419981cb47f3d207cb9ca18e69b3afd4980df
NothaboN <81325459+NothaboN@users.noreply.github.com>
2021-12-29 20:53:10 -0800

    removed console.log

--- src/app.js
+++ src/app.js
@@ -65,10 +65,10 @@ function displayForecast(response){
 }
 
 function getForecast(coordinates) {
-    console.log(coordinates);
+    
     let apiKey ="ee4ee350fa54faef06893e12edc706a5";
 let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
-console.log(apiUrl);
+
 axios.get(apiUrl).then(displayForecast);
 }
 
